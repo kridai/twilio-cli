fd2456b094dc7ed2fa5a1b13a4675ef72c3d1922
kridai <kridaigoswami@gmail.com>
2021-12-03 12:31:42 +0530

    Update pack_macos.sh

--- .github/scripts/pack_macos.sh
+++ .github/scripts/pack_macos.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
-#Functionality from signing macos package
-# create variables
+#Functionality for signing macos package
+
 
 import_certificate() {
-    echo "runner them is " $RUNNER_TEMP
     CERTIFICATE=$RUNNER_TEMP/certificate.p12
     OSX_KEYCHAIN=$RUNNER_TEMP/app-signing.keychain-db
     # import certificate from secrets
     echo -n "$OSX_INSTALLER_CERT_BASE64" | base64 --decode --output $CERTIFICATE
-    echo $OSX_INSTALLER_CERT_BASE64
-    echo $OSX_INSTALLER_CERT_PASS
-    # create temporary keychain
-    OSX_KEYCHAIN_PASSWORD="passphrase"
-    security list-keychains
+    # genrate random keychain password
+    OSX_KEYCHAIN_PASSWORD=`openssl rand -hex 12`
+    # create new keychain 
     security create-keychain -p "$OSX_KEYCHAIN_PASSWORD" $OSX_KEYCHAIN
     security unlock-keychain -p "$OSX_KEYCHAIN_PASSWORD" $OSX_KEYCHAIN 
+    # set keycahin configuration (lock after timeout etc)
     security set-keychain-settings -lut 21600 $OSX_KEYCHAIN
-    ls $RUNNER_TEMP
-    security list-keychains
+    # import certificate to keychain
     security import $CERTIFICATE -k $OSX_KEYCHAIN -f pkcs12 -A -T /usr/bin/codesign -T /usr/bin/security -P "$OSX_INSTALLER_CERT_PASSWORD"
     security set-key-partition-list -S apple-tool:,apple: -k "$OSX_KEYCHAIN_PASSWORD" $OSX_KEYCHAIN
+    
     security list-keychains -d user -s $OSX_KEYCHAIN login.keychain
     #security import $CERTIFICATE_PATH -k $KEYCHAIN_PATH -A -P $OSX_INSTALLER_CERT_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
     security find-identity
 }
 notarize_and_staple() {
     #Functionality  to notarize application
-    #xcrun notarytool store-credentials new-profile --apple-id "kgoswami@twilio.com" --password "tqsk-taur-nnau-kvxm" --team-id "VPZ4UMT2G9"
     xcrun notarytool store-credentials new-profile --apple-id "$APPLE_ID" --password "$APPLE_ID_APP_PASSWORD" --team-id "$APPLE_TEAM_ID"
+    # wait for notarization response and capture it in notarization_log.json
     xcrun notarytool submit "$FILE_PATH" --keychain-profile new-profile --wait -f json >> $RUNNER_TEMP/notarization_log.json
     notarization_status=$(jq -r .status $RUNNER_TEMP/notarization_log.json)
     notarization_id=$(jq -r .id $RUNNER_TEMP/notarization_log.json)
@@ -38,7 +36,7 @@ notarize_and_staple() {
       spctl --assess -vv --type install "$FILE_PATH"
     else
       echo "Notarization unsuccessfull"
-      #get notarization logs
+      #display notarization logs for error
       xcrun notarytool log ${notarization_id} --keychain-profile new-profile $RUNNER_TEMP/notarization_log.json
       jq . $RUNNER_TEMP/notarization_log.json
       exit 1
